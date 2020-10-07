2ba9b969f954cff63d122941cbc69f7f017949a4
elp49 <elp49@drexel.edu>
2020-10-07 08:33:38 -0400

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Edward Parrish
+
+This Readme gives detailing instructions to compile and run the ftpclient code.
---
+++ ftpclient.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Edward Parrish
+# ftpclient.py
+
+import sys
+
+
+def main():
+    print(len(sys.argv))
+
+
+if __name__ == "__main__":
+    main()
